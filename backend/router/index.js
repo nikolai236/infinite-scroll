5f381e5516e8e463b6501f02849d4adf49978444
piletonikolai <50989574+nikolai236@users.noreply.github.com>
2022-03-01 15:53:27 +0200

    Delete backend/router directory

--- backend/router/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router()
-
-const imageController = require('../controllers/imageController')
-
-router.get('/file/:filename', imageController.getSingleImage);
-router.get('/page/:num', imageController.getPage);
-router.delete('/:name', imageController.deleteImage);
-
-module.exports = router;
